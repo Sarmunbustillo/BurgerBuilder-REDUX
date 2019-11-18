e1e09eee72709a2262e27f9ed854a88d7dca8e1a
Sarmunbustillo <sarchamo26@gmail.com>
2019-11-18 16:41:48 +0100

    Updates

---
+++ precache-manifest.3c762bd4a84a0174d739d14cc40e4176.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55225c14af1d74d94dc7c804d4b0abc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79809cfbdcfb8fae61cd",
+    "url": "/static/css/3.2cdc48b4.chunk.css"
+  },
+  {
+    "revision": "f4e2a5a55d505e72fd12",
+    "url": "/static/css/4.4dff9618.chunk.css"
+  },
+  {
+    "revision": "b35a2382da63ac07f167",
+    "url": "/static/css/5.8ed8310f.chunk.css"
+  },
+  {
+    "revision": "5ef67bdb03c58201a091",
+    "url": "/static/css/main.f45aef06.chunk.css"
+  },
+  {
+    "revision": "3b82c5272fbd3af2bd8b",
+    "url": "/static/js/2.ed9cca8b.chunk.js"
+  },
+  {
+    "revision": "79809cfbdcfb8fae61cd",
+    "url": "/static/js/3.8956ec44.chunk.js"
+  },
+  {
+    "revision": "f4e2a5a55d505e72fd12",
+    "url": "/static/js/4.49f1f5ae.chunk.js"
+  },
+  {
+    "revision": "b35a2382da63ac07f167",
+    "url": "/static/js/5.7511fd1d.chunk.js"
+  },
+  {
+    "revision": "5ef67bdb03c58201a091",
+    "url": "/static/js/main.23ee49f8.chunk.js"
+  },
+  {
+    "revision": "c685888a28146cd7261a",
+    "url": "/static/js/runtime-main.48e9879f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
